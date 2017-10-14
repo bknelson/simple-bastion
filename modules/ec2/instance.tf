5b1b1d37a8976a3a1f7da630c571aa2f0c113da5
Ben Nelson (Contractor) <ben.nelson@dreambox.com>
2017-10-14 15:02:47 -0700

    First pass with all code

---
+++ modules/ec2/instance.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "bastion" {
+  ami = "${var.ami_id}"
+  instance_type = "t2.nano"
+  key_name = "${var.ssh_key_name}"
+  subnet_id = "${element(split(",", var.subnet_list), -1)}"
+  vpc_security_group_ids = ["${aws_security_group.bastion}"]
+  associate_public_ip_address = true
+
+  tags {
+    Name = "bastion-${var.name}"
+    Role = "bastion"
+  }
+}
+
+resource "aws_security_group" "bastion" {
+  name = "bastion"
+  description = "restricted by users's IP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.user_ip}/32"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_route53_record" "bastion" {
+  zone_id = "${data.aws_route53_zone.dns_zone.zone_id}"
+  name = "${var.name}.${data.aws_route53_zone.dns_zone.name}"
+  type = "A"
+  records = ["${aws_instance.bastion.public_ip}"]
+}
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,30 @@
+variable "ami_id" {
+  type = "string"
+  description = "AMI ID for bastion instance"
+  default = "ami-e689729e"
+}
+
+variable "ssh_key_name" {
+  type = "string"
+  description = "Key for default user"
+}
+
+variable "subnet_list" {
+  type = "string"
+  description = "Comma delimited list of subnets for location of bastion"
+}
+
+variable "user_ip" {
+  type = "string"
+  description = "User's IP"
+}
+
+variable "name" {
+  type = "string"
+  description = "Name suffix for bastion"
+}
+
+variable "zone_name" {
+  type = "string"
+  description = "Rt 53 zone name"
+}
