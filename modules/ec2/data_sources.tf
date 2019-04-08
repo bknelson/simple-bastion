be36e23d99b2e5eb2d535953cbd54040c118e65f
Ben Nelson <bknelson4@gmail.com>
2019-04-07 23:10:27 -0700

    Adding docker run command

--- modules/ec2/data_sources.tf
+++ modules/ec2/data_sources.tf
@@ -1,3 +1,10 @@
 data "aws_route53_zone" "dns_zone" {
   name = "${var.zone_name}"
 }
+
+data "template_file" "userdata" {
+  template = "${file("${path.module}/userdata.template")}"
+  vars = {
+    zone_name = "${var.zone_name}"
+  }
+}
